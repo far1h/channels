f2b6927f21f1e71b802bd19f835ccdc9080e70e4
far1h <farihmuhammad45@gmail.com>
2024-10-23 17:42:25 +0700

    Try to implement channels

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-func main (){
-	links := []string {
+func main() {
+	links := []string{
 		"http://google.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
@@ -14,17 +14,24 @@ func main (){
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
+	// blocking call
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep it's up"
 }
